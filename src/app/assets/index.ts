90c5325816074744db86be0c51ca08cffea5a2d5
Alish <alishallawadhi5@gmail.com>
2024-11-08 00:06:55 +0530

    basic side nav implementation

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,24 +1,42 @@
+// Images
 import LoginBackground from './images/login-bg.jpg'
 import NotificationIcon from './images/notification-icon.svg'
 import SearchIcon from './images/search-icon.svg'
 import Logo1 from './images/logo-1.svg'
 import Hamburger from './images/hamburger.svg'
+import UploadImageIcon from './images/upload-image-icon.svg';
+import DashboardIcon from './images/dashboard-icon.svg'
+import DashboardIconActive from './images/dashboard-icon-active.svg'
+import VendorManagementicon from './images/vendor-m-icon.svg'
+import VendorManagementiconActive from './images/vendor-m-icon-active.svg'
+
+//Mui Icons
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import UploadImageIcon from './images/upload-image-icon.svg';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 
 export {
+    //images
     LoginBackground,
     NotificationIcon,
     SearchIcon,
     Logo1,
     Hamburger,
+    UploadImageIcon,
+    DashboardIcon,
+    DashboardIconActive,
+    VendorManagementicon,
+    VendorManagementiconActive,
+
+    //Mui Icons
+    CheckCircleIcon,
     AddCircleOutlineOutlinedIcon,
     FileUploadOutlinedIcon,
     CloseOutlinedIcon,
-    UploadImageIcon,
-    CheckCircleIcon
+    ArrowDropDownIcon,
+    ArrowRightIcon
 }
--- src/app/components/SideNav.tsx
+++ src/app/components/SideNav.tsx
@@ -1,21 +1,59 @@
-import { Box } from '@mui/material'
 import React from 'react'
-import { Hamburger, Logo1 } from '../assets'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from '@mui/material'
+import { ArrowDropDownIcon, ArrowRightIcon, DashboardIcon, DashboardIconActive, Hamburger, Logo1, VendorManagementicon, VendorManagementiconActive } from '../assets'
 import Image from 'next/image'
 
 const SideNav = () => {
   return (
     <>
-      <Box className="w-full bg-primary h-[calc(100vh-32px)] rounded-xl px-4 pt-9">
-        <Box className="flex items-center justify-between">
-          <Box>
-            <Image src={Logo1} alt="logo" />
+      <Box className="w-full bg-primary h-[calc(100vh-32px)] rounded-xl px-4 pt-9 pb-4 flex flex-col justify-between">
+        <Box>
+          <Box className="flex items-center justify-between">
+            <Box>
+              <Image src={Logo1} alt="logo" />
+            </Box>
+            <Box className="cursor-pointer">
+              <Image src={Hamburger} alt="logo" />
+            </Box>
           </Box>
-          <Box className="cursor-pointer">
-            <Image src={Hamburger} alt="logo" />
+          <Box className='mt-9 flex flex-col gap-2'>
+            <Box className="flex items-center gap-[6px] p-3 rounded-lg cursor-pointer bg-primary300 nav-item active">
+              <Image src={DashboardIcon} alt='dashboard' className='nav-item-icon' />
+              <Image src={DashboardIconActive} alt='dashboard' className='nav-item-icon-active' />
+              <Typography className='text-sm text-white nav-item-label'>Dashboard</Typography>
+            </Box>
+            <Box>
+              <Accordion elevation={0}>
+                <AccordionSummary
+                  expandIcon={<ArrowDropDownIcon className='arrow-down-icon' />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                  // className='active'
+                >
+                  <Box className="flex items-center gap-[6px]">
+                    <Image src={VendorManagementicon} alt='vendor' className='vendor-m-icon' />
+                    <Image src={VendorManagementiconActive} alt='vendor' className='vendor-m-icon-active' />
+                    <Typography>Vendor Management</Typography>
+                  </Box>
+                </AccordionSummary>
+                <AccordionDetails className='mt-2'>
+                  <Box className="flex flex-col gap-[16px]">
+                    <Box className="flex gap-2 text-white cursor-pointer side-nav-child active">
+                      <ArrowRightIcon className='side-nav-child-icon' />
+                      <Typography className='side-nav-child-label'>Buyers</Typography>
+                    </Box>
+                    <Box className="flex gap-2 text-white cursor-pointer side-nav-child">
+                      <ArrowRightIcon className='side-nav-child-icon' />
+                      <Typography className='side-nav-child-label'>Suppliers</Typography>
+                    </Box>
+                  </Box>
+                  <Box></Box>
+                </AccordionDetails>
+              </Accordion>
+            </Box>
           </Box>
         </Box>
-
+        <Box>a</Box>
       </Box>
     </>
   )
