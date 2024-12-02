823cc5cd24f3e053f6c9da93727025f5f24e4d7f
Alish <alishallawadhi5@gmail.com>
2024-12-02 14:58:25 +0530

    notify menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,17 @@
-import { Box, InputAdornment, OutlinedInput} from '@mui/material'
+import { Box, IconButton, InputAdornment, Menu, MenuItem, OutlinedInput } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 import { NotificationIcon, HeaderSearchIcon } from '../assets'
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <Box className='bg-primary rounded-[12px] flex w-full justify-between items-center py-4 ps-6 pr-[18px] h-[72px]'>
@@ -20,7 +28,29 @@ const Header = () => {
             fullWidth
           />
         </Box>
-        <Box><Image src={NotificationIcon} alt='notify' /></Box>
+        <IconButton
+          aria-label="more"
+          id="long-button"
+          aria-controls={open ? 'long-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <Image src={NotificationIcon} alt='notify' />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            'aria-labelledby': 'long-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem>
+            abcd
+          </MenuItem>
+        </Menu>
       </Box>
     </>
   )
